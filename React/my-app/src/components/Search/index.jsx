4aa64910c4f2dbb8b74d483c1dc600e65ce95b66
WilburQ <createyx@gmail.com>
2022-05-16 23:47:03 +0800

    feat: reactdemo

--- React/my-app/src/components/Search/index.jsx
+++ React/my-app/src/components/Search/index.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import PubSub from 'pubsub-js'
+// import axios from 'axios'
 
 export default class Search extends Component {
 
-	search = ()=>{
+	search = async () => {
 		//获取用户的输入(连续解构赋值+重命名)
-		const {keyWordElement:{value:keyWord}} = this
+		const { keyWordElement: { value: keyWord } } = this
 		//发送请求前通知App更新状态
-		this.props.updateAppState({isFirst:false,isLoading:true})
-		//发送网络请求
-		axios.get(`/api1/search/users?q=${keyWord}`).then(
-			response => {
-				//请求成功后通知App更新状态
-				this.props.updateAppState({isLoading:false,users:response.data.items})
-			},
-			error => {
-				//请求失败后通知App更新状态
-				this.props.updateAppState({isLoading:false,err:error.message})
-			}
-		)
+		PubSub.publish('searchValue', { isFirst: false, isLoading: true })
+		//#region 发送网络请求
+		// axios.get(`/api1/search/users?q=${keyWord}`).then(
+		// 	response => {
+		// 		//请求成功后通知App更新状态
+		// 		PubSub.publish('searchValue', { isLoading: false, users: response.data.items })
+		// 	},
+		// 	error => {
+		// 		//请求失败后通知App更新状态
+		// 		PubSub.publish('searchValue ', { isLoading: false, err: error.message })
+		// 	}
+		// )
+		//#endregion
+		try {
+			const res = await fetch(`/api1/search/users?q=${keyWord}`)
+			const data = await res.json()
+			PubSub.publish('searchValue', { isLoading: false, users: data.items })
+		} catch (error) {
+			PubSub.publish('searchValue ', { isLoading: false, err: error.message })
+		}
 	}
 
 	render() {
@@ -26,7 +35,7 @@ export default class Search extends Component {
 			<section className="jumbotron">
 				<h3 className="jumbotron-heading">搜索github用户</h3>
 				<div>
-					<input ref={c => this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索"/>&nbsp;
+					<input ref={c => this.keyWordElement = c} type="text" placeholder="输入关键词点击搜索" />&nbsp;
 					<button onClick={this.search}>搜索</button>
 				</div>
 			</section>
